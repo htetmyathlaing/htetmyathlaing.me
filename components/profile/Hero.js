19dd73d4313d198cde42ab697fa54254d7a5d972
Htet Myat Hlaing <htetmyathlaing.dev@gmail.com>
2024-12-17 19:38:56 +0700

    feat: add email in contect

--- components/profile/Hero.js
+++ components/profile/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className="md:w-1/2 text-center md:text-left md:mr-8">
       <h1 className="text-5xl mb-4">I&apos;m <span className="font-josefin text-gradient"> Htet Myat</span>,</h1>
       <h1 className="text-3xl mb-4 text-gray-300">A Software Engineer</h1>
-      <Typewriter text="who wants to live peacefully..." className=" text-xl text-gray-300 font-josefin" />
+      <Typewriter text="who wants to live peacefully..." className="text-xl text-gray-300 font-josefin" />
       <div className="text-lg flex items-center justify-center md:justify-start font-montserrat">
         <span className="text-5xl text-white">7</span>
         <span className="text-5xl text-gradient">+</span>
