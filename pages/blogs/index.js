d4caca1333e92d1db8697ee55eca663ea75fe888
Htet Myat Hlaing <htetmyathlaing.dev@gmail.com>
2024-12-18 01:08:41 +0700

    feat: use view place holder while fetching blogs

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -17,27 +17,28 @@ export default function Blog() {
     fetchData();
   }, []);
 
-  if (loading) {
-    return (
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="text-2xl font-bold text-indigo-500">
-          <FaSpinner className="animate-spin" /> 
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className="min-h-screen p-4">
       <main className="container mx-auto p-4">
-        <ul>
-          {subPages.map(page => (
-            <li key={page.id} className="mb-4">
-              <Link href={`/blogs/${page.id}`} className="text-2xl font-bold text-indigo-500 ">{page.name}</Link>
-              <div className="text-sm text-gray-600">{page.date}</div>
-            </li>
-          ))}
-        </ul>
+        {loading ? (
+          <ul>
+            {Array.from({ length: 10 }).map((_, index) => (
+              <li key={index} className="mb-4 animate-pulse">
+                <div className="h-6 bg-indigo-500 rounded w-full md:w-3/4 mb-2"></div>
+                <div className="h-4 bg-gray-600 rounded w-16"></div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <ul>
+            {subPages.map(page => (
+              <li key={page.id} className="mb-4">
+                <Link href={`/blogs/${page.id}`} className="text-2xl font-bold text-indigo-500 ">{page.name}</Link>
+                <div className="text-sm text-gray-600">{page.date}</div>
+              </li>
+            ))}
+          </ul>
+        )}
       </main>
     </div>
   );
