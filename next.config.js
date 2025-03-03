5a683278d86cd654e6db39d65ba0cda80f4a6f68
Htet Myat Hlaing <htetmyathlaing.dev@gmail.com>
2025-03-03 13:24:37 +0700

    feat: add google analytic

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  publicRuntimeConfig: {
+    GA_TRACKING_ID: process.env.GA_TRACKING_ID,
+  },
+};
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+// Add this import to use environment variables
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
+
 class MyDocument extends Document {
   render() {
     return (
@@ -9,6 +14,20 @@ class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=Montserrat:wght@400;700&family=Josefin+Sans:wght@600&display=swap"
             rel="stylesheet"
+          />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${publicRuntimeConfig.GA_TRACKING_ID}`}
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${publicRuntimeConfig.GA_TRACKING_ID}');
+              `,
+            }}
           />
         </Head>
         <body>
